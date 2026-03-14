6c18b72e8c8fa5dbf5afb4e19cd6ecd3e74e08c6
Bhim pd Lamichhane <51348717+bhimpd@users.noreply.github.com>
2026-03-14 20:14:42 +0545

    Add assertNumericInputValue method to Helper class
    Added assertNumericInputValue method for numeric validation.

--- utils/helper.ts
+++ utils/helper.ts
@@ -1,54 +1,53 @@
 import { Page, Locator, expect } from "@playwright/test";
 
-export class Helper{
-    readonly page:Page;
-    readonly columnHeaderSelector:Locator;
+export class Helper {
+    readonly page: Page;
+    readonly columnHeaderSelector: Locator;
 
 
-    constructor(page:Page){
-        this.page=page;
+    constructor(page: Page) {
+        this.page = page;
         this.columnHeaderSelector = page.locator("span.pq-title-span");
 
     }
 
-    async visitPage(expectedUrl:string){
+    async visitPage(expectedUrl: string) {
         await this.page.goto(expectedUrl);
     }
 
-    async assertURL(expectedUrl:string){
+    async assertURL(expectedUrl: string) {
         await expect(this.page).toHaveURL(expectedUrl);
     }
 
-    async assertText(expectedSelector:Locator, expectedText:string)
-    {
+    async assertText(expectedSelector: Locator, expectedText: string) {
         const actualText = await expectedSelector.textContent();
+        console.log("Actual Text ==> ", actualText);
+        console.log("Expected Text ==> ", expectedText);
         expect(actualText?.trim()).toBe(expectedText.trim());
     }
 
-    async fillSearchParameterNClick(expectedSelector:Locator, expectedText:any)
-    {
+    async fillSearchParameterNClick(expectedSelector: Locator, expectedText: any) {
         await expectedSelector.fill(expectedText);
-        await this.page.waitForTimeout(2000);
+        await expect(expectedSelector).toHaveValue(expectedText);
+
         await expectedSelector.press("Enter");
-        await this.page.waitForTimeout(2000);
+        // await this.page.waitForTimeout(1500);
+
     }
 
 
-    async assertTextOnInputValue(expectedSelector:Locator, expectedText:string)
-    {
+    async assertTextOnInputValue(expectedSelector: Locator, expectedText: string) {
         const actualText = await expectedSelector.inputValue();
         expect(actualText?.trim()).toBe(expectedText.trim());
     }
 
 
-    async assertTextOnPlaceholder(expectedSelector:Locator, expectedText:string)
-    {
+    async assertTextOnPlaceholder(expectedSelector: Locator, expectedText: string) {
         const placeholder = await expectedSelector.getAttribute('placeholder');
         expect(placeholder).toBe(expectedText);
     }
 
-    async assertUrl(expectedSelector:Locator, expectedUrl:string)
-    {
+    async assertUrl(expectedSelector: Locator, expectedUrl: string) {
         const placeholder = await expectedSelector.getAttribute('href');
         expect(placeholder).toBe(expectedUrl);
     }
@@ -58,20 +57,27 @@ export class Helper{
         const length = await header.count();
         console.log("Lenght of the Columns Headers.....", length);
 
-         if (length !== expectedTexts.length) {
+        if (length !== expectedTexts.length) {
             throw new Error(`Header count mismatch: expected ${expectedTexts.length}, got ${length}`);
         }
 
-        for (let i=0; i<=length-1; i++){
-            const columnText = (await header.nth(i).innerText()).trim(); 
+        for (let i = 0; i <= length - 1; i++) {
+            const columnText = (await header.nth(i).innerText()).trim();
             console.log(`Header ${i}:`, columnText);
 
             await expect(header.nth(i)).toHaveText(expectedTexts[i]);
-            
+
         }
     }
 
 
+    async assertNumericInputValue(expectedSelector: Locator, expectedValue: any) {
+        const actualValue = await expectedSelector.inputValue();
+
+        console.log("Actual Amount ==> ", actualValue);
+        console.log("Expected Amount ==> ", expectedValue);
+        expect(Number(actualValue)).toBeCloseTo(Number(expectedValue), 2);
+    }
 
 
 }
